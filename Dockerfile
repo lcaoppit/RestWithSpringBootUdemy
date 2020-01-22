39b1f6af793f22fe6ce2a10a7759f9b47b885144
Luis Araujo <luis.araujo@oppit.com.br>
2020-01-22 13:02:30 -0300

    New Release

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM mysql:5.7.29
+EXPOSE 3308
--- SpringBootRestApi/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ SpringBootRestApi/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,4 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('leandro', 'Leandro Costa', '$2a$16$9qr2tv0HmXbHBsx.TZFjfux742wCZM32a8Wi6iBqwIqaizlHPuxHS', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', '$2a$16$h4yDQCYTy62R6xrtFDWONeMH3Lim4WQuU/aj8hxW.dJJoeyvtEkhK', b'1', b'1', b'1', b'1');
+	('sysadmin', 'Sysadmin', '$2a$16$9qr2tv0HmXbHBsx.TZFjfux742wCZM32a8Wi6iBqwIqaizlHPuxHS', b'1', b'1', b'1', b'1'),
+	('laraujo', 'Luis Aeaujo', '$2y$12$Vft3jrptJ2zEyM3g2rmJIufwhYigWBnWFOtEaZeb01gqoO9j0A9x2', b'1', b'1', b'1', b'1'),
+	('devsecops', 'DevSecOps', '$2a$16$h4yDQCYTy62R6xrtFDWONeMH3Lim4WQuU/aj8hxW.dJJoeyvtEkhK', b'1', b'1', b'1', b'1');
